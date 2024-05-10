c21461ca093fa1e21c580666ad2454ab3b5571c7
ABDUL AKRAM A <143951232+Abdul-Akram-A@users.noreply.github.com>
2024-05-10 19:49:10 +0530

    fix: Update main.js
    In This Update:  Ristrict the choice

--- Rock,Paper,Scissor/main.js
+++ Rock,Paper,Scissor/main.js
@@ -2,30 +2,37 @@ let intro=confirm("Shall we play Rock,Paper,Scissors");
 if (intro){
     let userChoice=prompt("Please enter Rock,Paper,Scissors").trim().toLowerCase();
     if (userChoice){
-        const choices=["rock","paper","scissors"];
-        let computerChoice=choices[Math.floor(Math.random()*choices.length)]
-        let result= 
-        computerChoice === userChoice 
-        ? "Draw" 
-        : computerChoice ===  "rock" 
-        ? userChoice ===  "paper" 
-            ? "You Winz" 
-            : "Computer Winz"
-        : computerChoice === "paper"
-        ? userChoice === "scissors"
-            ? "You Winz"
-            : "Computer Winz"
-        : computerChoice === "scissors"
-        ? userChoice === "rock"
-            ? "You Winz"
-            : "Computer Winz"
-        :"Computer Winz"
-        alert(`The computer choice ${computerChoice} \n Player choice ${userChoice} \n ${result}`)
-        let playAgain=confirm("Play Again?");
-        playAgain ? location.reload() : alert("Goodbye");
-    }
+        if (userChoice === "rock" ||
+            userChoice === "paper" ||
+            userChoice === "scissors"
+        ){
+            const choices=["rock","paper","scissors"];
+            let computerChoice=choices[Math.floor(Math.random()*choices.length)]
+            let result= 
+            computerChoice === userChoice 
+            ? "Draw" 
+            : computerChoice ===  "rock" 
+            ? userChoice ===  "paper" 
+                ? "You Winz" 
+                : "Computer Winz"
+            : computerChoice === "paper"
+            ? userChoice === "scissors"
+                ? "You Winz"
+                : "Computer Winz"
+            : computerChoice === "scissors"
+            ? userChoice === "rock"
+                ? "You Winz"
+                : "Computer Winz"
+            :"Computer Winz"
+            alert(`The computer choice ${computerChoice} \n Player choice ${userChoice} \n ${result}`)
+            let playAgain=confirm("Play Again?");
+            playAgain ? location.reload() : alert("Goodbye");
+        }
+        else{
+            alert("Enter Invalid Choice");
+        }}
     else{
-        alert("You didn't enter anything");
+        alert("Enter Nothing")
     }
 }
 else{
